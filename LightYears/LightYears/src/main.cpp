2c246268a29def8220d3efcaaec49ddfbaf614a8
Shmael13 <liamsi@Na-na-na-na-Batman>
2025-05-26 17:12:21 +0500

    Stopped using SFML, using Allegro instead

--- LightYears/LightYears/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <SFML/Graphics.hpp>
-
-int main() {
-    sf::RenderWindow window(sf::VideoMode(sf::Vector2u(800, 600)), "MyWindow");
-
-  window.setFramerateLimit(60);
-
-}
